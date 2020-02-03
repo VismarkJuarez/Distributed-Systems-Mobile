0ab0264dcd975f8fd558e49ab7dd6d58e4fd5d86
Vismark Juarez <vismarkjuarez@gmailcom>
2020-02-02 23:22:08 -0600

    Refactored the main activity UI to prepare it for consuming JSON data from the instructor server.

--- app/src/main/java/com/vismark/distributedsystems/loyola/testapp/MainActivity.java
+++ app/src/main/java/com/vismark/distributedsystems/loyola/testapp/MainActivity.java
@@ -21,74 +21,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Creating references to each of the 4 buttons
-        Button firstButton = (Button)findViewById(R.id.button1);
-        Button secondButton = (Button)findViewById(R.id.button2);
-        Button thirdButton = (Button)findViewById(R.id.button3);
-        Button fourthButton = (Button)findViewById(R.id.button4);
+        Button firstButton = (Button)findViewById(R.id.submitButton);
+
 
         //Setting onClick behaviour
         firstButton.setOnClickListener(new buttonOneClicked());
-        secondButton.setOnClickListener(new buttonTwoClicked());
-        thirdButton.setOnClickListener(new buttonThreeClicked());
-        fourthButton.setOnClickListener(new buttonFourClicked());
     }
 
     //OnClick button handler classes
     private class buttonOneClicked implements View.OnClickListener {
         @Override
         public void onClick(View v) {
-            incrementAnswer1Count();
-        }
-    }
-
-    private class buttonTwoClicked implements View.OnClickListener {
-        @Override
-        public void onClick(View v) {
-            incrementAnswer2Count();
-        }
-    }
-    private class buttonThreeClicked implements View.OnClickListener {
-        @Override
-        public void onClick(View v) {
-            incrementAnswer3Count();
-        }
-    }
-    private class buttonFourClicked implements View.OnClickListener {
-        @Override
-        public void onClick(View v) {
-            incrementAnswer4Count();
+            submitAnswer();
         }
     }
 
     //Ancillary methods
-    private void incrementAnswer1Count() {
+    private void submitAnswer() {
         Toast.makeText(getApplicationContext(),
-                "Button1 has been pressed.",
+                "Answer has been submitted.",
                 Toast.LENGTH_LONG)
                 .show();
 
         sendNetworkRequest("This is an example payload");
-    }
-
-    private void incrementAnswer2Count() {
-        Toast.makeText(getApplicationContext(),
-                "Button2 has been pressed.",
-                Toast.LENGTH_LONG)
-                .show();
-    }
-
-    private void incrementAnswer3Count() {
-        Toast.makeText(getApplicationContext(),
-                "Button3 has been pressed.",
-                Toast.LENGTH_LONG)
-                .show();
-    }
-
-    private void incrementAnswer4Count() {
-        Toast.makeText(getApplicationContext(),
-                "Button4 has been pressed.",
-                Toast.LENGTH_LONG)
-                .show();
     }
 
     private void sendNetworkRequest(String data) {
