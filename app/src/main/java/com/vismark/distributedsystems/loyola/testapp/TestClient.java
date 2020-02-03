317b04eb53980e50333eef6cd967e3cd2c6faaa9
Vismark Juarez <vismarkjuarez@gmailcom>
2020-02-02 23:29:01 -0600

    Added a new method in the TestClient interface for Retrofi to retrieve new questions from the instructor server.

--- app/src/main/java/com/vismark/distributedsystems/loyola/testapp/TestClient.java
+++ app/src/main/java/com/vismark/distributedsystems/loyola/testapp/TestClient.java
@@ -2,10 +2,14 @@ package com.vismark.distributedsystems.loyola.testapp;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 
 public interface TestClient {
     @PUT("question1")
     Call<String> updateAnswer1Count(@Body String data);
+
+    @GET("submitAnswer")
+    Call<String> submitAnswer(@Body Answer answer);
 }
