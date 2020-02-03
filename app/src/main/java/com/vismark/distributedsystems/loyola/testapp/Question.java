4eaf10053b015f070c3d8353071d55ee39dc84ae
Vismark Juarez <vismarkjuarez@gmailcom>
2020-02-02 23:49:05 -0600

    Created a class for encapsulating Question details and another for encapsulating all Questions returned from the instructor server.  These will come in handy when fetching data from the instructor server

---
+++ app/src/main/java/com/vismark/distributedsystems/loyola/testapp/Question.java
@@ -0,0 +1,59 @@
+package com.vismark.distributedsystems.loyola.testapp;
+
+public class Question {
+    private String questionType;
+    private int questionId;
+    private String question;
+    private String multipleChoiceOption1;
+    private String multipleChoiceOption2;
+    private String multipleChoiceOption3;
+
+    public void setQuestionType(String questionType) {
+        this.questionType = questionType;
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setMultipleChoiceOption1(String multipleChoiceOption1) {
+        this.multipleChoiceOption1 = multipleChoiceOption1;
+    }
+
+    public void setMultipleChoiceOption2(String multipleChoiceOption2) {
+        this.multipleChoiceOption2 = multipleChoiceOption2;
+    }
+
+    public void setMultipleChoiceOption3(String multipleChoiceOption3) {
+        this.multipleChoiceOption3 = multipleChoiceOption3;
+    }
+
+    public String getQuestionType() {
+        return questionType;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getMultipleChoiceOption1() {
+        return multipleChoiceOption1;
+    }
+
+    public String getMultipleChoiceOption2() {
+        return multipleChoiceOption2;
+    }
+
+    public String getMultipleChoiceOption3() {
+        return multipleChoiceOption3;
+    }
+
+}
---
+++ app/src/main/java/com/vismark/distributedsystems/loyola/testapp/Questions.java
@@ -0,0 +1,20 @@
+package com.vismark.distributedsystems.loyola.testapp;
+
+import java.util.List;
+
+public class Questions {
+    private List<Question> questions;
+
+    public Questions(List<Question> questions) {
+        this.questions = questions;
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
+    
+}
