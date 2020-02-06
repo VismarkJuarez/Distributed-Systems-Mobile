af83a68c4450231a7b892ddd2ba4847839bd6810
VismarkJuarez <vismarkjuarez@gmail.com>
2020-02-06 14:16:31 -0600

    Added two new activities -- one to serve as the Instructor landing page, another for a Student landing page.

---
+++ app/src/main/java/com/loyola/distributedsystems439/spring2020/testapp/InstructorHome.java
@@ -0,0 +1,13 @@
+package com.loyola.distributedsystems439.spring2020.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InstructorHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructor_home);
+    }
+}
---
+++ app/src/main/java/com/loyola/distributedsystems439/spring2020/testapp/StudentHome.java
@@ -0,0 +1,13 @@
+package com.loyola.distributedsystems439.spring2020.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StudentHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_home);
+    }
+}
