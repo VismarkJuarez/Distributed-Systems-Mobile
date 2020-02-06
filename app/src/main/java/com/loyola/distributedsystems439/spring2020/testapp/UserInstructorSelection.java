83d70fbc3cd29a250b377e62a8686d51e104c56b
VismarkJuarez <vismarkjuarez@gmail.com>
2020-02-06 14:52:27 -0600

    Wrote logic in preparation for opening a new activity for when the user clicks on the instructorButton or the studentButton.

--- app/src/main/java/com/loyola/distributedsystems439/spring2020/testapp/UserInstructorSelection.java
+++ app/src/main/java/com/loyola/distributedsystems439/spring2020/testapp/UserInstructorSelection.java
@@ -2,21 +2,53 @@ package com.loyola.distributedsystems439.spring2020.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class UserInstructorSelection extends AppCompatActivity {
 
-    //Storing references to the instructor and student buttons
-    private Button studentButton = (Button)findViewById(R.id.studentButton);
-    private Button instructorButton = (Button)findViewById(R.id.instructorButton);
+    //Creating references to the instructor and student buttons
+    private Button STUDENT_BUTTON;
+    private Button INSTRUCTOR_BUTTON;
+
+    private void setup() {
+        //Initializing button references
+        STUDENT_BUTTON = (Button)findViewById(R.id.studentButton);
+        INSTRUCTOR_BUTTON = (Button)findViewById(R.id.instructorButton);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_instructor_selection);
 
+        setup();
 
-
-
+        //setting onClick behaviour
+        STUDENT_BUTTON.setOnClickListener(new studentButtonClicked());
+        INSTRUCTOR_BUTTON.setOnClickListener(new instructorButtonClicked());
     }
+
+    //OnClick button handler classes
+    private class studentButtonClicked implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(),
+                    "StudentButton clicked!",
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+    private class instructorButtonClicked implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(),
+                    "InstructorButton clicked!",
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
 }
