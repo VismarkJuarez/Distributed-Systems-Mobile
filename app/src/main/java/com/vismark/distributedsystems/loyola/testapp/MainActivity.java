97c263c38ad9ca3a1b2475c7b771303731106f33
Vismark Juarez <vismarkjuarez@gmailcom>
2020-02-06 00:30:08 -0600

    Wrote the logic for displaying a new activity called .

--- app/src/main/java/com/vismark/distributedsystems/loyola/testapp/MainActivity.java
+++ app/src/main/java/com/vismark/distributedsystems/loyola/testapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.vismark.distributedsystems.loyola.testapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
@@ -31,6 +33,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, SplashScreenActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
 
         Button loadQuestionsButton = (Button)findViewById(R.id.loadQuestionsButton);
         loadQuestionsButton.setOnClickListener(new loadQuestionsButtonClicked());
