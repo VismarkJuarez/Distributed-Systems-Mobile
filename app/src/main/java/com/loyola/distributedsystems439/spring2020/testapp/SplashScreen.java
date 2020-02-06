4ad0693e0123a5a856ee67c42191a9c1088cf3e1
VismarkJuarez <vismarkjuarez@gmail.com>
2020-02-06 14:00:03 -0600

    Wrote logic for displaying the UserInstructorSelection intent after the splash screen.

--- app/src/main/java/com/loyola/distributedsystems439/spring2020/testapp/SplashScreen.java
+++ app/src/main/java/com/loyola/distributedsystems439/spring2020/testapp/SplashScreen.java
@@ -18,7 +18,7 @@ public class SplashScreen extends AppCompatActivity {
 
             @Override
             public void run() {
-                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                Intent i = new Intent(SplashScreen.this, UserInstructorSelection.class);
                 startActivity(i);
                 finish();
             }
--- app/src/main/java/com/loyola/distributedsystems439/spring2020/testapp/UserInstructorSelection.java
+++ app/src/main/java/com/loyola/distributedsystems439/spring2020/testapp/UserInstructorSelection.java
@@ -9,5 +9,7 @@ public class UserInstructorSelection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_instructor_selection);
+
+
     }
 }
