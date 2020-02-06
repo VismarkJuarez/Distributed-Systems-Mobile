7b7b95e4c8f7e3facc96344813acef956c5fa9ee
VismarkJuarez <vismarkjuarez@gmail.com>
2020-02-06 01:44:03 -0600

    Wrote SplayScreen timeout and display logic.

--- app/src/main/java/com/vismark/distributedsystems/loyola/testapp/SplashScreen.java
+++ app/src/main/java/com/vismark/distributedsystems/loyola/testapp/SplashScreen.java
@@ -1,13 +1,27 @@
 package com.vismark.distributedsystems.loyola.testapp;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN_TIMEOUT_MILLIS = 4000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIMEOUT_MILLIS);
     }
 }
