15b52d14e895e0af5d931185a45eb11bfcc834d2
VismarkJuarez <vismarkjuarez@gmail.com>
2020-02-06 14:25:15 -0600

    Added references to the student and instructor buttons.

--- app/src/main/java/com/loyola/distributedsystems439/spring2020/testapp/UserInstructorSelection.java
+++ app/src/main/java/com/loyola/distributedsystems439/spring2020/testapp/UserInstructorSelection.java
@@ -2,13 +2,20 @@ package com.loyola.distributedsystems439.spring2020.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class UserInstructorSelection extends AppCompatActivity {
+
+    //Storing references to the instructor and student buttons
+    private Button studentButton = (Button)findViewById(R.id.studentButton);
+    private Button instructorButton = (Button)findViewById(R.id.instructorButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_instructor_selection);
+
+
 
 
     }
