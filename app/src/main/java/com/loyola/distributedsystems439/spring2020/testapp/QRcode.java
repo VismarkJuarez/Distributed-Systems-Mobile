615f5b482ac21ea2c8f7cd317f6c5238f2ad0163
ddpalacios <palaciosdanieldario@gmail.com>
2020-02-20 12:04:24 -0600

    Set up user permission to access camera

---
+++ app/src/main/java/com/loyola/distributedsystems439/spring2020/testapp/QRcode.java
@@ -0,0 +1,72 @@
+package com.loyola.distributedsystems439.spring2020.testapp;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class QRcode extends AppCompatActivity {
+    SurfaceView cameraPreview;
+    TextView txtResult;
+    BarcodeDetector barcodeDetector;
+    CameraSource cameraSource;
+    int RequestCameraPermissionID = 1001;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qrcode);
+
+        cameraPreview = (SurfaceView) findViewById(R.id.camera_preview);
+        txtResult = (TextView) findViewById(R.id.txtResult);
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .build();
+
+        //Add Event
+        cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(QRcode.this,
+                            new String[]{Manifest.permission.CAMERA}, RequestCameraPermissionID);
+
+
+                    return;
+                }
+                try {
+                    cameraSource.start(cameraPreview.getHolder());
+
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+
+            }
+        });
+
+    }
+}
